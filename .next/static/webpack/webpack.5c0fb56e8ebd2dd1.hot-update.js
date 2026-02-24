6fd208b7194e19f3dea3ae28598359ef1eca612d
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2026-02-23 21:03:41 -0600

    Attempt to fix sidebar scrolling issue

--- .next/static/webpack/webpack.5c0fb56e8ebd2dd1.hot-update.js
+++ .next/static/webpack/webpack.5c0fb56e8ebd2dd1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "392819a45f8a3a38"; }
+/******/ 	__webpack_require__.h = function() { return "e7fd604924d66860"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
